c7927e21c6f7a1176ae6878afd248a2339a681bd
Влад <vlad.kozlov20032003@gmail.com>
2022-10-01 16:52:17 +0300

    3

--- user/user/settings.py
+++ user/user/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'pages',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -127,3 +129,4 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
