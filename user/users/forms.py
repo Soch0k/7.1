b58674e081b989eb08bb83d266df49c5047c6003
Влад <vlad.kozlov20032003@gmail.com>
2022-10-05 06:26:28 +0300

    45

--- user/users/forms.py
+++ user/users/forms.py
@@ -2,12 +2,12 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
+
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
-        Model = CustomUser
-        fields = ('username', 'email')
-
+        model = CustomUser
+        fields = ('username', 'email',)
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = ('username', 'email')
+        fields = ('username', 'email',)
